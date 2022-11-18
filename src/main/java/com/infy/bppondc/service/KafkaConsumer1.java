fc3ac615573a28e04ce874e3eec0441cd3dc9f1d
shivanijha179 <shivanihit179@gmailcom>
2022-11-18 12:58:21 +0530

    Kafka checks with table data

--- src/main/java/com/infy/bppondc/service/KafkaConsumer1.java
+++ src/main/java/com/infy/bppondc/service/KafkaConsumer1.java
@@ -2,9 +2,12 @@ package com.infy.bppondc.service;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.infy.bppondc.service.dto.CartDTO;
 import com.infy.bppondc.service.dto.Catalog;
 import com.infy.bppondc.service.dto.Item;
+import com.infy.bppondc.service.dto.ProductDTO;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +18,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaConsumer1 {
 
+    private final ProductService productService;
+
     @Autowired
     KafkaTemplate<String, Catalog> kafkaTemplate;
 
     private static final String TOPIC = "newtopicresponse";
+
+    public KafkaConsumer1(ProductService productService) {
+        this.productService = productService;
+    }
 
     @KafkaListener(topics = "publishtopic", groupId = "group_id1")
     public void consumer1(String message) {
@@ -28,23 +37,49 @@ public class KafkaConsumer1 {
 
         System.out.println("Message from Producer to consumer1 !!!!!= " + message);
 
-        Map<String, String> map = Map.of("100", "Milk", "102", "Eggs", "101", "Bread");
-
         String resultProduct = name.substring(1, name.length() - 1);
 
-        if (map.containsValue(resultProduct)) {
-            System.out.println(
-                "The item requested by BAP : " + resultProduct + " is present here in BPP 1. Sending the Items back to the BAP in catalog."
-            );
+        List<ProductDTO> productDTO = productService.findAll();
 
-            Item itm = new Item("1001", "Consumer 1 Replied :" + resultProduct);
+        for (int i = 0; i < productDTO.size(); i++) {
+            Map<String, String> map = new HashMap<>();
+            if (productDTO.get(i).getTitle().equals(resultProduct) && (productDTO.get(i).getStore().getId() == 1001)) {
+                System.out.println(
+                    "The item requested by BAP : " +
+                    resultProduct +
+                    " is present here in BPP 1. Sending the Items back to the BAP in catalog."
+                );
+                map.put("ProductName", productDTO.get(i).getTitle());
 
-            List lst = new ArrayList<>();
-            lst.add(itm);
-            Catalog obj = new Catalog(id, lst);
-            kafkaTemplate.send(TOPIC, obj);
-        } else {
-            System.out.println("The item requested by BAP :" + resultProduct + " is not present here in BPP 1");
+                Item itm = new Item("1001", "Consumer 1 Replied :" + resultProduct);
+
+                List lst = new ArrayList<>();
+                lst.add(itm);
+                Catalog obj = new Catalog(id, lst);
+                kafkaTemplate.send(TOPIC, obj);
+            } else {
+                // System.out.println("The item requested by BAP :" + resultProduct + " is not present here in BPP 1");
+            }
         }
+        //
+        //        Map<String, String> map = Map.of("100", "Milk", "102", "Eggs", "101", "Bread");
+        //
+        //
+        //        if (map.containsValue(resultProduct)) {
+        //            System.out.println(
+        //                "The item requested by BAP : " + resultProduct + " is present here in BPP 1. Sending the Items back to the BAP in catalog."
+        //            );
+        //
+        //            Item itm = new Item("1001", "Consumer 1 Replied :" + resultProduct);
+        //
+        //            List lst = new ArrayList<>();
+        //            lst.add(itm);
+        //            Catalog obj = new Catalog(id, lst);
+        //            kafkaTemplate.send(TOPIC, obj);
+        //        } else {
+        //            System.out.println("The item requested by BAP :" + resultProduct + " is not present here in BPP 1");
+        //        }
+        //
+
     }
 }
--- src/main/java/com/infy/bppondc/service/KafkaConsumer2.java
+++ src/main/java/com/infy/bppondc/service/KafkaConsumer2.java
@@ -4,7 +4,9 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.infy.bppondc.service.dto.Catalog;
 import com.infy.bppondc.service.dto.Item;
+import com.infy.bppondc.service.dto.ProductDTO;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,41 +17,64 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaConsumer2 {
 
+    private final ProductService productService;
+
     @Autowired
     KafkaTemplate<String, Catalog> kafkaTemplate;
 
     private static final String TOPIC = "newtopicresponse";
 
+    public KafkaConsumer2(ProductService productService) {
+        this.productService = productService;
+    }
+
     @KafkaListener(topics = "publishtopic", groupId = "group_id2")
     public void consumer1(String message) {
         System.out.println("Message from Producer to consumer2 @@@@@= " + message);
-        //        Item itm=new Item(UUID.randomUUID().toString(),"Champak");
-        //        List lst=new ArrayList<>();
-        //        lst.add(itm);
-        //        Catalog obj=new Catalog(UUID.randomUUID().toString(),lst);
-        //        kafkaTemplate.send(TOPIC, obj);
 
         JsonObject jsonObject = new JsonParser().parse(message).getAsJsonObject();
         String name = String.valueOf(jsonObject.get("productName"));
         String id = jsonObject.get("productId").toString();
 
-        Map<String, String> map = Map.of("100", "Milk", "102", "Eggs", "101", "Bread", "103", "Yogurt");
-
         String resultProduct = name.substring(1, name.length() - 1);
 
-        if (map.containsValue(resultProduct)) {
-            System.out.println(
-                "The item requested by BAP : " + resultProduct + " is present here in BPP 2. Sending the Items back to the BAP in catalog"
-            );
+        List<ProductDTO> productDTO = productService.findAll();
 
-            Item itm = new Item("1002", "Consumer 2 Replied :" + resultProduct);
+        for (int i = 0; i < productDTO.size(); i++) {
+            Map<String, String> map = new HashMap<>();
+            if (productDTO.get(i).getTitle().equals(resultProduct) && (productDTO.get(i).getStore().getId() == 1002)) {
+                System.out.println(
+                    "The item requested by BAP : " +
+                    resultProduct +
+                    " is present here in BPP 2. Sending the Items back to the BAP in catalog."
+                );
+                map.put("ProductName", productDTO.get(i).getTitle());
 
-            List lst = new ArrayList<>();
-            lst.add(itm);
-            Catalog obj = new Catalog(id, lst);
-            kafkaTemplate.send(TOPIC, obj);
-        } else {
-            System.out.println("The item requested by BAP :" + resultProduct + " is not present here in BPP 2");
+                Item itm = new Item("1002", "Consumer 2 Replied :" + resultProduct);
+
+                List lst = new ArrayList<>();
+                lst.add(itm);
+                Catalog obj = new Catalog(id, lst);
+                kafkaTemplate.send(TOPIC, obj);
+            } else {
+                // System.out.println("The item requested by BAP :" + resultProduct + " is not present here in BPP 1");
+            }
         }
+        //        Map<String, String> map = Map.of("100", "Milk", "102", "Eggs", "101", "Bread", "103", "Yogurt");
+        //        if (map.containsValue(resultProduct)) {
+        //            System.out.println(
+        //                "The item requested by BAP : " + resultProduct + " is present here in BPP 2. Sending the Items back to the BAP in catalog"
+        //            );
+        //
+        //            Item itm = new Item("1002", "Consumer 2 Replied :" + resultProduct);
+        //
+        //            List lst = new ArrayList<>();
+        //            lst.add(itm);
+        //            Catalog obj = new Catalog(id, lst);
+        //            kafkaTemplate.send(TOPIC, obj);
+        //        } else {
+        //            System.out.println("The item requested by BAP :" + resultProduct + " is not present here in BPP 2");
+        //        }
+
     }
 }
